265d8fff7d44acd63a3e73b88e9386577315b4e1
Mitchell R <support@bettercorp.co.za>
2022-03-27 15:14:41 +0200

    build flow updates

--- Clients/build/ci-desktop-build.sh
+++ Clients/build/ci-desktop-build.sh
@@ -32,6 +32,12 @@ if [[ -d "./out/make/darwin/" ]]; then
   echo "$1" >./out/make/darwin/x64/version.txt
   tar -czvf "$RELD/SimpleWeb-Desktop-darwin-x64-$1.tar.gz" ./out/make/darwin/x64/*.*
 fi
+if [[ -d "./out/make/zip/" ]]; then
+  echo "Output make release darwin"
+  cp ../../License/* ./out/make/zip/x64/
+  echo "$1" >./out/make/zip/x64/version.txt
+  tar -czvf "$RELD/SimpleWeb-Desktop-darwin-x64-$1.tar.gz" ./out/make/zip/x64/*.*
+fi
 if [[ -d "./out/make/squirrel.windows/" ]]; then
   echo "Output make release windows"
   cp ../../License/* ./out/make/squirrel.windows/x64/
