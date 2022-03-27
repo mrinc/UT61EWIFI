fbd6676a428e34c3fccd54fe03292dad58cf13e4
Mitchell R <support@bettercorp.co.za>
2022-03-27 15:32:25 +0200

    Updating dir flows to make output tars folder clean

--- Clients/build/ci-desktop-build.sh
+++ Clients/build/ci-desktop-build.sh
@@ -19,33 +19,35 @@ if [[ -d "./out/make/deb/" ]]; then
   ls ./out/make/deb;
   cp ../../License/* ./out/make/deb/x64/
   echo "$1" >./out/make/deb/x64/version.txt
-  tar -czvf "./out/release/SimpleWeb-Desktop-deb-x64-$1.tar.gz" ./out/make/deb/x64/*.*
+  cd ./out/make/deb/x64;
+  tar -czvf "../../../../out/release/SimpleWeb-Desktop-deb-x64-$1.tar.gz" ./*.*
 fi
 if [[ -d "./out/make/rpm/" ]]; then
   echo "Output make release rpm"
   ls ./out/make/rpm;
   cp ../../License/* ./out/make/rpm/x64/
   echo "$1" >./out/make/rpm/x64/version.txt
-  tar -czvf "./out/release/SimpleWeb-Desktop-rpm-x64-$1.tar.gz" ./out/make/rpm/x64/*.*
+  cd ./out/make/rpm/x64;
+  tar -czvf "../../../../out/release/SimpleWeb-Desktop-rpm-x64-$1.tar.gz" ./*.*
 fi
-if [[ -d "./out/make/darwin/" ]]; then
-  echo "Output make release darwin"
-  ls ./out/make/darwin;
-  cp ../../License/* ./out/make/darwin/x64/
-  echo "$1" >./out/make/darwin/x64/version.txt
-  tar -czvf "./out/release/SimpleWeb-Desktop-darwin-x64-$1.tar.gz" ./out/make/darwin/x64/*.*
-fi
+
 if [[ -d "./out/make/zip/" ]]; then
-  echo "Output make release darwin"
+  echo "Output make release zip"
   ls ./out/make/zip;
-  cp ../../License/* ./out/make/zip/x64/
-  echo "$1" >./out/make/zip/x64/version.txt
-  tar -czvf "./out/release/SimpleWeb-Desktop-darwin-x64-$1.tar.gz" ./out/make/zip/x64/*.*
+  if [[ -d "./out/make/zip/darwin/" ]]; then
+    echo "Output make release darwin"
+    ls ./out/make/zip/darwin;
+    cp ../../License/* ./out/make/zip/darwin/x64/
+    echo "$1" >./out/make/zip/darwin/x64/version.txt
+    cd ./out/make/zip/darwin/x64;
+    tar -czvf "../../../../../out/release/SimpleWeb-Desktop-darwin-x64-$1.tar.gz" ./*.*
+  fi
 fi
 if [[ -d "./out/make/squirrel.windows/" ]]; then
   echo "Output make release windows"
   ls ./out/make/squirrel.windows;
   cp ../../License/* ./out/make/squirrel.windows/x64/
   echo "$1" >./out/make/squirrel.windows/x64/version.txt
-  tar -czvf "./out/release/SimpleWeb-Desktop-windows-x64-$1.tar.gz" ./out/make/squirrel.windows/x64/*.*
+  cd ./out/make/squirrel.windows/x64;
+  tar -czvf "../../../../out/release/SimpleWeb-Desktop-windows-x64-$1.tar.gz" ./*.*
 fi
